2fadaaccfdfa5f2b5a44a991efde4542e6a4d4e2
clairep9 <clairepetitfrere@pursuit.org>
2024-04-19 23:25:26 -0400

    created dummy data for drivers to access their locations.

---
+++ truck-map/database.sql
@@ -0,0 +1,41 @@
+\c swiftswirl
+
+CREATE TABLE drivers (
+    driver_id  SERIAL PRIMARY KEY NOT NULL,
+    firstname VARCHAR(100) NOT NULL,
+    lastname
+     VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    driver_id INT REFERENCES drivers(driver_id),
+    latitude DECIMAL(9, 6) NOT NULL,
+    longitude DECIMAL(9, 6) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO drivers (firstname, lastname, email)
+VALUES
+    ('John', 'Garcia', 'jgarcia@gmail.com'),
+    ('Alex', 'Grey', 'agrey@gmail.com'),
+    ('Zoe', 'Bryant', 'zoebryant@aol.com'),
+    ('Brianna', 'Lafontaine', 'brianna_l18@hotmail.com'),
+    ('Cory', 'Vasquez', 'cvasquez@gmail.com'),
+    ('Chase', 'Sergey', 'csergey1@gmail.com'),
+    ('Ciara', 'Laurent', 'ciaralaurent@gmail.com'),
+    ('Bryan', 'Green', 'bryangreen@gmail.com');
+
+
+INSERT INTO locations (driver_id, latitude, longitude)
+VALUES
+    (1, 40.6782, -73.9442),  -- Brooklyn Heights
+    (2, 40.6501, -73.9496),  -- Flatbush
+    (3, 40.6622, -73.9851),  -- Park Slope
+    (4, 40.6842, -73.9653),  -- Williamsburg
+    (5, 40.6310, -74.0300),  -- Bay Ridge
+    (6, 40.6262, -74.0307),  -- Sunset Park
+    (7, 40.7056, -73.9787),  -- Downtown Brooklyn
+    (8, 40.6786, -73.9771);  -- Brooklyn Navy Yard
